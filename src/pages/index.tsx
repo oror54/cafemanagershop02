3fa37e8886009805552e1dfba36d4b12e511fe11
hong jihyeon <hjh@eplatform.co.kr>
2024-12-06 19:07:52 +0900

    메인 섹션 02 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Section01 from './main/Section01';
+import Section02 from './main/Section02';
 
 export default function Home() {
   return (
     <main>
       <Section01 />
+      <Section02 />
     </main>
   );
 }
