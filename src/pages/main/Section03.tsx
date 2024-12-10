a7dd37b553bb2cc2c6d8a644f1d4f04c2c4a617e
hong jihyeon <hjh@eplatform.co.kr>
2024-12-10 18:20:02 +0900

    링크이동

--- src/pages/main/Section03.tsx
+++ src/pages/main/Section03.tsx
@@ -5,25 +5,27 @@ import styles from "./Main.module.css";
 export default function Section03() {
     return (
         <section className={styles.section03}>
-        <div className={styles.wrap}>
-            <div className={styles.brandBox}>
-                <div className={styles.tit}>
-                    <div className={styles.titPoint}>
-                        <span>CSFEMANAGER</span>
+            <div className={styles.wrap}>
+                <div className={styles.brandBox}>
+                    <div className={styles.tit}>
+                        <div className={styles.titPoint}>
+                            <span>CSFEMANAGER</span>
+                        </div>
+                        <h3>카페매니저의 다양한 서비스를 통해<br />
+                            카페 운영의 어려움을 해결해 보세요!</h3>
                     </div>
-                    <h3>카페매니저의 다양한 서비스를 통해<br />
-                        카페 운영의 어려움을 해결해 보세요!</h3>
-                </div>
-                <div className={styles.linkWrap}>
-                    <button className={styles.link}>
-                        <i className={`${styles.logo} ${styles.apple}`}></i>
-                    </button>
-                    <button className={styles.link}>
-                        <i className={`${styles.logo} ${styles.google}`}></i>
-                    </button>
+                    <div className={styles.linkWrap}>
+                        <button className={styles.link}
+                            onClick={() => window.location.href = 'https://apps.apple.com/kr/app/%EC%B9%B4%ED%8E%98%EB%A7%A4%EB%8B%88%EC%A0%80/id6504548363'}>
+                            <i className={`${styles.logo} ${styles.apple}`}></i>
+                        </button>
+                        <button className={styles.link}
+                            onClick={() => window.location.href = 'https://play.google.com/store/apps/details?id=com.cuport.cafemanager&pli=1'}>
+                            <i className={`${styles.logo} ${styles.google}`}></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
