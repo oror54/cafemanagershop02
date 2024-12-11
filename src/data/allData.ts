944d0b52b759b224fcf3023e9a07b7b2224f0bac
hong jihyeon <hjh@eplatform.co.kr>
2024-12-11 15:25:15 +0900

    중배전 이미지 오류 수정

--- src/data/allData.ts
+++ src/data/allData.ts
@@ -43,7 +43,6 @@ import mediumRoast16 from "$/assets/images/sub/beans/mid/item16_랑케_킵고잉
 import mediumRoastDetail16 from "$/assets/images/sub/beans/mid/detail16_랑케_킵고잉.png"
 
 import mediumRoast17 from "$/assets/images/sub/beans/mid/item17_GSH_C블랜딩.png"
-import mediumRoastDetail17 from "$/assets/images/sub/beans/mid/detail17_GSH_C블랜딩.jpg"
 import mediumRoast18 from "$/assets/images/sub/beans/mid/item18_GSH_G블랜딩.png"
 import mediumRoastDetail18 from "$/assets/images/sub/beans/mid/detail18_GSH_G블랜딩.jpg"
 import mediumRoast19 from "$/assets/images/sub/beans/mid/item19_GSH_H블랜딩.png"
@@ -596,7 +595,7 @@ export const allData: dataType[] = [
     name: "C블랜딩",
     brand: "GSHCOFFEE",
     imageUrl: mediumRoast17,
-    description: mediumRoastDetail17,
+    description: defaultImage,
     categoryKo: '원두',
     categoryDetail: '중배전',
     url: "",
