97e97dcd5b8766665f5657a74b93697c604c8c4d
hong jihyeon <hjh@eplatform.co.kr>
2024-12-11 15:11:12 +0900

    원두 중배전

---
+++ src/data/imageAssets.ts
@@ -0,0 +1,41 @@
+import milk01 from "$/assets/images/sub/fresh/milk/item01_서울우유신림5동_우유.png";
+import dairy01 from "$/assets/images/sub/fresh/dairy/item01_서울우유신림5동_생크림.png";
+import dairy02 from "$/assets/images/sub/fresh/dairy/item02_서울우유신림5동_연유.png";
+import freshothers01 from "$/assets/images/sub/fresh/others/item01_빈커스텀컴퍼니_181라운지더치커피.png";
+import freshothersDetail01 from "$/assets/images/sub/fresh/others/detail01_빈커스텀컴퍼니_181라운지더치커피.jpg";
+import freshotherNewBg01 from "$/assets/images/sub/fresh/others/item01_빈커스텀컴퍼니_181라운지더치커피_newbg.jpg"
+import freshothers02 from "$/assets/images/sub/fresh/others/item02_빈커스텀컴퍼니_181라운지디카페인더치커피.png";
+import freshothersDetail02 from "$/assets/images/sub/fresh/others/detail02_빈커스텀컴퍼니_181라운지디카페인더치커피.jpg";
+import freshothers03 from "$/assets/images/sub/fresh/others/item03_GSHCOFFEE_예가체프콜드브루.png";
+import freshothersDetail03 from "$/assets/images/sub/fresh/others/detail03_GSHCOFFEE_예가체프콜드브루.jpg";
+import freshothers04 from "$/assets/images/sub/fresh/others/item04_로스팅코리아_콜드브루디카페인.png";
+import freshothersDetail04 from "$/assets/images/sub/fresh/others/detail04_로스팅코리아_콜드브루디카페인.jpg";
+import freshothers05 from "$/assets/images/sub/fresh/others/item05_로스팅코리아_콜드브루블렌딩.png";
+import freshothersDetail05 from "$/assets/images/sub/fresh/others/detail05_로스팅코리아_콜드브루블렌딩.jpg";
+import freshothers06 from "$/assets/images/sub/fresh/others/item06_솔룸커피로스터스_솔룸다크콜드브루원액.png";
+import freshothersDetail06 from "$/assets/images/sub/fresh/others/detail06_솔룸커피로스터스_솔룸다크골드브루원액.png";
+import freshothers07 from "$/assets/images/sub/fresh/others/item07_솔룸커피로스터스_콜롬비아디카페인콜드브루원액.png";
+import freshothersDetail07 from "$/assets/images/sub/fresh/others/detail07_솔룸커피로스터스_콜롬비아디카페인골드브루원액.png";
+import defaultImage from "$/default-image.png";
+
+export const images = {
+    milk01,
+    dairy01,
+    dairy02,
+    freshothers01,
+    freshothersDetail01,
+    freshotherNewBg01,
+    freshothers02,
+    freshothersDetail02,
+    freshothers03,
+    freshothersDetail03,
+    freshothers04,
+    freshothersDetail04,
+    freshothers05,
+    freshothersDetail05,
+    freshothers06,
+    freshothersDetail06,
+    freshothers07,
+    freshothersDetail07,
+    defaultImage,
+};
