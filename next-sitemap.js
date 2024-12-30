a0f42d41297e6d05bdb87dec709b8b3c3b68b6f3
hong jihyeon <hjh@eplatform.co.kr>
2024-12-30 13:53:22 +0900

    sitemap 추가

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://www.cafemanagershop.com',  // Replace with your domain
+    generateRobotsTxt: true,  // Optional: generate robots.txt file
+    changefreq: 'weekly',  // Optional: set how often the pages change
+    priority: 0.7,  // Optional: set default priority for pages
+    sitemapSize: 7000,  // Optional: set the maximum number of URLs per sitemap file
+};
