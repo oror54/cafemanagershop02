3ab6d647b643e5c3cddcf726efa85f01aad5c95a
hong jihyeon <hjh@eplatform.co.kr>
2025-01-03 09:45:14 +0900

    인스타그램 이동 수정

--- src/data/instagramPosts.tsx
+++ src/data/instagramPosts.tsx
@@ -5,10 +5,10 @@ import insta03 from "$/assets/images/main/section06/insta03.jpg";
 import insta04 from "$/assets/images/main/section06/insta04.jpg";
 
 export const instagramPosts = [
-    { id: 1, imageSrc: insta01, link: "#" },
-    { id: 2, imageSrc: insta02, link: "#" },
-    { id: 3, imageSrc: insta03, link: "#" },
-    { id: 4, imageSrc: insta04, link: "#" },
+    { id: 1, imageSrc: insta01, link: "https://www.instagram.com/cafemanager_official" },
+    { id: 2, imageSrc: insta02, link: "https://www.instagram.com/cafemanager_official" },
+    { id: 3, imageSrc: insta03, link: "https://www.instagram.com/cafemanager_official" },
+    { id: 4, imageSrc: insta04, link: "https://www.instagram.com/cafemanager_official" },
 ].map((post) => ({
     ...post,
     alt: "카페매니저 인스타그램",
