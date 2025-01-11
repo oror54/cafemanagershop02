060294a5acec7eea8d0fe85d3ffca39327e8070b
jihyeon <jihyeon33@gmail.com>
2025-01-11 16:32:16 +0900

    meta 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ export default function Document() {
 
         {/* SEO Meta Tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="원두, 디저트 등 다양한 카페재료를 최저가로 견적받고 손쉽게 구입해보세요. 그리고 카페사장님을 위한 다양한 혜택도 무료로 받아가세요." />
+        <meta name="description" content="원두, 디저트 등 다양한 카페재료를 최저가 견적으로 손쉽게 구입할 수 있는 카페매니저입니다. 카페사장님을 위한 다양한 혜택을 무료로 받아가세요" />
         <meta name="keywords" content="카페원두, 원두, 디저트, 시럽, 원두납품, 원두도매, 카페재료, 카페시럽, 디저트납품, 카페우유, 카페원두납품, 디저트레시피, 카페창업, 사업자원두, 업소용원두, 카페원두납품, 개인카페창업, 카페, 베이커리납품, 소자본카페" />
         <meta name="author" content="https://www.cafemanagershop.com" />
         <meta name="Robots" content="index, follow" />
@@ -30,10 +30,11 @@ export default function Document() {
         <meta name="Googlebot" content="index,follow" />
         <meta name="daumoa" content="index,follow" />
         <meta name="format-detection" content="telephone=no" />
+        <meta name="twitter:description" content="원두, 디저트 등 다양한 카페재료를 최저가 견적으로 손쉽게 구입할 수 있는 카페매니저입니다. 카페사장님을 위한 다양한 혜택을 무료로 받아가세요." />
 
         {/* Open Graph (OG) Meta Tags */}
         <meta property="og:title" content="사업자 전용 최저가 납품 쇼핑몰 - 카페매니저" />
-        <meta property="og:description" content="원두, 디저트 등 다양한 카페재료를 최저가로 견적받고 손쉽게 구입해보세요. 그리고 카페사장님을 위한 다양한 혜택도 무료로 받아가세요." />
+        <meta property="og:description" content="원두, 디저트 등 다양한 카페재료를 최저가 견적으로 손쉽게 구입할 수 있는 카페매니저입니다. 카페사장님을 위한 다양한 혜택을 무료로 받아가세요." />
         <meta property="og:image" content="/ogp.jpg" />
         <meta property="og:url" content="https://www.cafemanagershop.com" />
         <meta property="og:type" content="website" />
