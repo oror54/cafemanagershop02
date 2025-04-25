40f2b636874297da053637679d44601ecfc49d5e
jihyeon <jihyeon33@gmail.com>
2025-04-25 20:31:54 +0900

    메타태그추가 검색엔진 및 스크립트 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,44 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+       {/* Google Tag Manager */}
+       <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WFWBGTZT');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+         {/* Meta Pixel Code */}
+         <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1263665402080556');
+  fbq('track', 'PageView');`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
+
+
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -20,9 +55,7 @@ export default function Document() {
         <meta name="description" content="원두, 디저트 등 다양한 카페재료를 최저가 견적으로 손쉽게 구입할 수 있는 카페매니저입니다. 카페사장님을 위한 다양한 혜택을 무료로 받아가세요" />
         <meta name="keywords" content="카페원두, 원두, 디저트, 시럽, 원두납품, 원두도매, 카페재료, 카페시럽, 디저트납품, 카페우유, 카페원두납품, 디저트레시피, 카페창업, 사업자원두, 업소용원두, 카페원두납품, 개인카페창업, 카페, 베이커리납품, 소자본카페" />
         <meta name="author" content="https://www.cafemanagershop.com" />
-        <meta name="Robots" content="index, follow" />
-        <meta name="naver-site-verification" content="" />
-        <meta name="google-site-verification" content=" " />
+        <meta name="robots" content="index, follow" />
         <meta name="NaverBot" content="All" />
         <meta name="NaverBot" content="index,follow" />
         <meta name="Yeti" content="All" />
@@ -46,6 +79,30 @@ export default function Document() {
         <meta name="google-site-verification" content="iY8MNvrBwCorFcJBU8-rfiSpSssR4gfTfofiaFDcoUY" />
       </Head>
       <body>
+         {/* Google Tag Manager (noscript) */}
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WFWBGTZT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1263665402080556&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
+
+        
         <Main />
         <NextScript />
       </body>
