7538e1d7c795bcf3820b8cd9d436087d818f165e
jihyeon <jihyeon33@gmail.com>
2025-04-25 22:14:36 +0900

    메타 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,40 +5,6 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-       {/* Google Tag Manager */}
-       <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WFWBGTZT');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-
-         {/* Meta Pixel Code */}
-         <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1263665402080556');
-  fbq('track', 'PageView');`,
-          }}
-        />
-        {/* End Meta Pixel Code */}
-
-
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -53,13 +19,11 @@ export default function Document() {
         {/* SEO Meta Tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="원두, 디저트 등 다양한 카페재료를 최저가 견적으로 손쉽게 구입할 수 있는 카페매니저입니다. 카페사장님을 위한 다양한 혜택을 무료로 받아가세요" />
-        <meta name="keywords" content="카페원두, 원두, 디저트, 시럽, 원두납품, 원두도매, 카페재료, 카페시럽, 디저트납품, 카페우유, 카페원두납품, 디저트레시피, 카페창업, 사업자원두, 업소용원두, 카페원두납품, 개인카페창업, 카페, 베이커리납품, 소자본카페" />
+        <meta name="keywords" content="카페원두, 원두, 디저트, 시럽, 원두납품, 원두도매, 카페재료, 카페시럽, 디저트납품, 카페우유, 카페원두납품, 디저트레시피, 카페창업, 사업자원두, 업소용원두, 개인카페창업, 카페, 베이커리납품, 소자본카페" />
         <meta name="author" content="https://www.cafemanagershop.com" />
         <meta name="robots" content="index, follow" />
         <meta name="NaverBot" content="All" />
-        <meta name="NaverBot" content="index,follow" />
         <meta name="Yeti" content="All" />
-        <meta name="Yeti" content="index,follow" />
         <meta name="Googlebot" content="index,follow" />
         <meta name="daumoa" content="index,follow" />
         <meta name="format-detection" content="telephone=no" />
@@ -79,8 +43,8 @@ export default function Document() {
         <meta name="google-site-verification" content="iY8MNvrBwCorFcJBU8-rfiSpSssR4gfTfofiaFDcoUY" />
       </Head>
       <body>
-         {/* Google Tag Manager (noscript) */}
-         <noscript>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WFWBGTZT"
             height="0"
@@ -88,7 +52,6 @@ export default function Document() {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
         {/* Meta Pixel (noscript) */}
         <noscript>
@@ -100,11 +63,39 @@ export default function Document() {
             alt=""
           />
         </noscript>
-        {/* End Meta Pixel (noscript) */}
 
-        
         <Main />
         <NextScript />
+
+        {/* 아래에 Script 추가 */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WFWBGTZT');`,
+          }}
+        />
+
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1263665402080556');
+  fbq('track', 'PageView');`,
+          }}
+        />
       </body>
     </Html>
   );
